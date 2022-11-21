ce72082a8c1d64596aa90bd2e88b5ef74ae0bc7f
JulieSalden <108523952+JulieSalden@users.noreply.github.com>
2022-11-21 15:27:02 +0100

    Add files via upload

--- assignementFilmFinder/script.js
+++ assignementFilmFinder/script.js
@@ -1,29 +1,72 @@
 const moviesUl = document.querySelector("#movieList");
 
-const addMoviesToDom = (movies) => {
-  movies.map((movies) => {
+//algemene functie om de lijsten naar de dom te verplaatsen
+//zorg dat je lijst met Li's terug krijgt
+//creeer en plaats de image voor in de Li
+//Li's toevoegen aan Ul
+const addMoviesToDom = (array) => {
+  array.map((movie) => {
     const newLi = document.createElement("li");
-    newLi.innerHTML = movies.title;
+    const moviePoster = document.createElement("img");
+    moviePoster.src = movie.poster;
+    newLi.appendChild(moviePoster);
     moviesUl.appendChild(newLi);
   });
-};
+}; //// dit werkt ///////
 
 addMoviesToDom(movies);
 
+//filter functie voor jaartal boven 2014
+const latestMovies = movies.filter((movie) => {
+  return movie.year == "2014", movie.year > "2014";
+});
+console.log(latestMovies); ///// dit werkt ////
 
-// Iterate through all the movies in the movie list. That takes this function as an argument.
-//------ dus wat bedoelt men hier? movies.forEach(addMoviesToDom)? en waar moet je dat plaatsen?
+//change event om nieuwe films naar de dom te sturen
+const latestMovieButton = document.querySelector("#latestMoviesButton");
+latestMovieButton.addEventListener("change", (e) => {
+  addMoviesToDom(latestMovies); 
+  /// addMoviesToDom WERKT NIET WHYYYYYY???-----------------------------------??
+});
 
 
-// First, you want to "convert" each movie into a complete <li> element(a Dom node) with a .map function.
-// We then put it into a variable. The result from the .map function will be an array with <li> elements.
-// ---- bedoelt men hier dat er nu uit deze functie zoals ik hem geschreven heb dit klopt of had ik
-// var app = document.querySelector('#app');
-// app.innerHTML = '<ul>' + wizards.map(function (wizard) {
-//	return '<li>' + wizard + '</li>';
-// }).join('') + '</ul>';
-// voorbeeld van internet, moeten gebruiken? 
+//event dat gaat rollen wanneer filterButton geklikt wordt en filter functie starten en toevoegen lijst DOM
+const handleOnChangeEvent = (event) => {
+  const choosenTitle = event.target.value;
+  console.log(choosenTitle);
+  switch (choosenTitle) {
+    case "avenger":
+      const avengerMovies = movies.map((movie) => {
+        return movies.filter((movie) => movie.title.includes("Avenger"));
+      });
+      console.log(avengerMovies);
+      addMoviesToDom(avengerMovies);
+      break;
+    case "xmen":
+      const xmenMovies = movies.filter((movie) => {
+        return movies.filter((movie) => movie.title.includes("X-men"));
+      });
+      addMoviesToDom(xmenMovies);
+      break;
+    case "princess":
+      const princessMovies = movies.filter((movie) => {
+        return movies.filter((movie) => movie.title.includes("Princess"));
+      });
+      addMoviesToDom(princessMovies);
+      break;
+    case "batman":
+      const batmanMovies = movies.filter((movie) => {
+        return movies.filter((movie) => movie.title.includes("Batman"));
+      });
+      addMoviesToDom(batmanMovies);
+      break;
+  }
+}; /// DIT WERKT NIETTT ---------------------------------------------??
 
-// Step 4: Outside the .map function: place a newly made array with <li> elements in the DOM by adding each li to the ul. Our suggestion: Use a .forEach loop.
-// ---- hoezo outside de map function? en waarom moet er nog een forEach komen hier?
-
+// add eventlistener op titleButtons buttons met zelfde class
+document.addEventListener("DOMContentLoaded", function () {
+  const titleButtons = document.querySelectorAll('input[class="titleButtons"]');
+  const titleButton = titleButtons.forEach(function (titleButton) {
+    titleButton.addEventListener("change", handleOnChangeEvent);
+  });
+}); ///// dit werkt //////
